b4e87627b8299ed84191da8ea30e813d20ca0c2d
Jun Kimura <jun.kimura@datachain.jp>
2021-08-17 15:17:24 +0900

    use the interface defined in connection
    Signed-off-by: Jun Kimura <jun.kimura@datachain.jp>

--- modules/proxy/keeper/client_keeper.go
+++ modules/proxy/keeper/client_keeper.go
@@ -2,25 +2,25 @@ package keeper
 
 import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
-	clientkeeper "github.com/cosmos/ibc-go/modules/core/02-client/keeper"
+	connectiontypes "github.com/cosmos/ibc-go/modules/core/03-connection/types"
 	"github.com/cosmos/ibc-go/modules/core/exported"
 	multivtypes "github.com/datachainlab/ibc-proxy/modules/light-clients/xx-multiv/types"
 )
 
-// ClientKeeper override `GetSelfConsensusState` and `ValidateSelfClient` in the keeper of ibc-client
+// ClientKeeper override `ValidateSelfClient` in the keeper of ibc-client
 // Original method doesn't yet support a consensus state for a general client
 type ClientKeeper struct {
-	clientkeeper.Keeper
+	connectiontypes.ClientKeeper
 }
 
-func NewClientKeeper(k clientkeeper.Keeper) ClientKeeper {
-	return ClientKeeper{Keeper: k}
+func NewClientKeeper(k connectiontypes.ClientKeeper) ClientKeeper {
+	return ClientKeeper{ClientKeeper: k}
 }
 
 func (k ClientKeeper) ValidateSelfClient(ctx sdk.Context, clientState exported.ClientState) error {
 	cs, ok := clientState.(*multivtypes.ClientState)
 	if !ok {
-		return k.Keeper.ValidateSelfClient(ctx, clientState)
+		return k.ClientKeeper.ValidateSelfClient(ctx, clientState)
 	}
-	return k.Keeper.ValidateSelfClient(ctx, cs.GetBaseClientState())
+	return k.ClientKeeper.ValidateSelfClient(ctx, cs.GetBaseClientState())
 }
