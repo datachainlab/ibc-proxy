5c8778094460f5a456b8cba8e63ca2a3a4d282f7
Jun Kimura <jun.kimura@datachain.jp>
2021-12-22 17:21:41 +0900

    remove unnecessary interfaces
    Signed-off-by: Jun Kimura <jun.kimura@datachain.jp>

--- modules/proxy/types/expected_keepers.go
+++ modules/proxy/types/expected_keepers.go
@@ -7,9 +7,5 @@ import (
 
 type ClientKeeper interface {
 	ClientStore(ctx sdk.Context, clientID string) sdk.KVStore
-	SetClientState(ctx sdk.Context, clientID string, clientState exported.ClientState)
 	GetClientState(ctx sdk.Context, clientID string) (exported.ClientState, bool)
-	SetClientConsensusState(ctx sdk.Context, clientID string, height exported.Height, consensusState exported.ConsensusState)
-	GetClientConsensusState(ctx sdk.Context, clientID string, height exported.Height) (exported.ConsensusState, bool)
-	CreateClient(ctx sdk.Context, clientState exported.ClientState, consensusState exported.ConsensusState) (string, error)
 }
